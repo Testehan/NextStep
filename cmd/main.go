a0617a5ff7188d3ecae5de078180585358e6b6c1
DanTestehan <tdan89@yahoo.com>
2026-01-11 20:20:20 +0100

    nextstep - fixed cors issue

--- cmd/main.go
+++ cmd/main.go
@@ -52,7 +52,7 @@ func main() {
 
 	// CORS Middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000"},
+		AllowOrigins:     []string{"http://testdan.com", "http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
